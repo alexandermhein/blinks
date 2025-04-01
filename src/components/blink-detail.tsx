c94281011a00b4a23a6680f54ff89bb5fffbaa26
Alexander Hein <alexandermhein@gmail.com>
2025-04-01 11:53:35 +0800

    feat: show source URL as hostname in blink detail

--- src/components/blink-detail.tsx
+++ src/components/blink-detail.tsx
@@ -1,12 +1,22 @@
-import { ActionPanel, Action, Icon, Detail } from "@raycast/api";
+import { ActionPanel, Action, Icon, Detail, useNavigation } from "@raycast/api";
 import { memo } from "react";
 import { BlinkDetailProps } from "../types/blinks";
 import { getBlinkTitle } from "../utils/design";
 import { formatDate } from "../utils/date";
 
 const BlinkDetail = memo(({ blink, onDelete }: BlinkDetailProps) => {
+  const { pop } = useNavigation();
   const markdown = `## ${blink.title}
-${blink.type === "quote" && blink.description ? `\n${blink.description}` : ""}`;
+
+${blink.description ? blink.description : ""}`;
+
+  const getHostname = (url: string) => {
+    try {
+      return new URL(url).hostname;
+    } catch {
+      return url;
+    }
+  };
 
   return (
     <Detail
@@ -17,13 +27,14 @@ ${blink.type === "quote" && blink.description ? `\n${blink.description}` : ""}`;
             <Detail.Metadata.TagList.Item text={getBlinkTitle(blink.type)} />
           </Detail.Metadata.TagList>
           {blink.source && (
-            <Detail.Metadata.Link title="Source" text={blink.source} target={blink.source} />
+            <Detail.Metadata.Link 
+              title="Source" 
+              text={getHostname(blink.source)} 
+              target={blink.source} 
+            />
           )}
           {blink.author && (
             <Detail.Metadata.Label title="Author" text={blink.author} />
-          )}
-          {blink.description && blink.type !== "quote" && (
-            <Detail.Metadata.Label title="Context" text={blink.description} />
           )}
           <Detail.Metadata.Label title="Created" text={formatDate(blink.createdOn)} />
         </Detail.Metadata>
@@ -37,7 +48,10 @@ ${blink.type === "quote" && blink.description ? `\n${blink.description}` : ""}`;
             icon={Icon.Trash}
             style={Action.Style.Destructive}
             shortcut={{ modifiers: ["cmd"], key: "backspace" }}
-            onAction={() => onDelete(blink.id)}
+            onAction={() => {
+              onDelete(blink.id);
+              pop();
+            }}
           />
         </ActionPanel>
       }
